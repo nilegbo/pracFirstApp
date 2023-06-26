7d20195dfa5e3ffc9b0db1412b4abacbbc053e81
nil <nilegbo@gmail.com>
2023-06-26 10:30:33 -0700

    Initial Commit

---
+++ pracFirstApp/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  pracFirstApp
+//
+//  Created by Nil Egbo on 6/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    var body: some View {
+        VStack {
+          Text("What is your name?")
+          TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+            textTitle = ""
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+        }
+    }
+    
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pracFirstApp/pracFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pracFirstAppApp.swift
+//  pracFirstApp
+//
+//  Created by Nil Egbo on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct pracFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
